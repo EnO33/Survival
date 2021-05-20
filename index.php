787a55b308bdfc8963403f885019b81bc0406b1d
EnO33 <73072830+EnO33@users.noreply.github.com>
2021-05-20 14:12:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
+session_start();
 require_once("core/includes.php");
+
+$url = htmlspecialchars($_SERVER['REQUEST_URI']);
+$url_array = explode('/', $url);
+
+$view = "public/views/" . htmlspecialchars($url_array[2]) . ".php";
+
+if (is_file($view)) {
+	include $view;
+}
