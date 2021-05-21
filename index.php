51bb6c207729d41db134f0ad6c0a76a71e105c59
EnO33 <73072830+EnO33@users.noreply.github.com>
2021-05-21 09:23:20 +0200

    include fixed navbar

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ $url_array = explode('/', $url);
 
 $view = "public/views/" . htmlspecialchars($url_array[2]) . ".view.php";
 
+include ("public/views/layout/layout_top.php");
+
+if (empty($url_array[2])) {
+	$view = "public/views/home.view.php";
+}
+
 if (is_file($view)) {
 	include $view;
 }
+
+include ("public/views/layout/layout_bottom.php");
--- public/views/layout/layout_bottom.php
+++ public/views/layout/layout_bottom.php
@@ -1,2 +1,10 @@
 	</body>
+	<script type="text/javascript" src="public/assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="public/assets/js/materialize.min.js"></script>
+
+	<script>
+		$(document).ready(function() {
+			$('.sidenav').sidenav();
+		});
+	</script>
 </html>
